30cd1b9402f1a325ec2172e082cb9c27635b427f
ValdezEdith <zedlavhtide@gmail.com>
2024-10-03 22:26:00 +0800

    Update taskCalendar.js

--- taskCalendar.js
+++ taskCalendar.js
@@ -1,6 +1,4 @@
 let day = "Monday";
-
-// Using a switch statement to map each day to a specific task
 switch(day) {
     case 'Monday':
         console.log("Your task for Monday is: Finish your activities");
@@ -8,7 +6,6 @@ switch(day) {
     case 'Tuesday':
         console.log("Your task for Tuesday is: Attend orientation");
         break;
-    // Add cases for other days as needed
     default:
         console.log("No task scheduled for " + day);
         break;
